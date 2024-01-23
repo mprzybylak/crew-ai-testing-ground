5b63ee60de2974bbb409a2089be8d1dad3ee44ab
Michal Przybylak <michal.przybylak@gmail.com>
2024-01-23 18:57:47 -0500

    Dummy hello world example

---
+++ hellow-world/hello-world.py
@@ -0,0 +1,31 @@
+from crewai import Agent, Task, Crew, Process
+from langchain.tools import DuckDuckGoSearchRun
+
+search_tool = DuckDuckGoSearchRun()
+
+researcher = Agent(
+    role='Researcher',
+    goal='Searching internet to find relevant info',
+    backstory='''
+    You have many years of experience doing research on internet.
+    ''',
+    verbose=True,
+    allow_delegation=False,
+    tools=[search_tool]
+)
+
+task = Task(
+    description='Find information about best trombone brands',
+    agent=researcher
+)
+
+crew = Crew(
+    agents=[researcher],
+    tasks=[task],
+    verbose=2
+)
+
+result = crew.kickoff()
+
+print("##########")
+print(result)
